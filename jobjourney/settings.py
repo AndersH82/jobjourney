20ffdc199019a2883f3dcec4049a2bf90309a1b5
AndersH82 <andersthereseholst@gmail.com>
2024-07-14 13:04:51 +0200

    add statics files, create the profile html

--- jobjourney/settings.py
+++ jobjourney/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'resume',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'resume/assets'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
